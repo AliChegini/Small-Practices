fb29494247306dc2ef13e23d6ef6f7847fc2866c
Ali Chegini <ali.c.engineer@gmail.com>
2019-01-27 13:54:40 +0100

    initial commit

---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Ehsan on 27/01/2019.
+//  Copyright © 2019 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Project is loaded ...")
+    }
+
+
+}
+
