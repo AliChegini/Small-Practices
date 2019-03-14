3fc75b258364541c5e654be47a5865d7c5dbe40f
Ali Chegini <ali.c.engineer@gmail.com>
2019-03-14 17:17:59 +0100

    collectionView practice

---
+++ Round1/Day2/CollectionViewPractice/CollectionViewPractice/HomeController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeController.swift
+//  CollectionViewPractice
+//
+//  Created by Ehsan on 14/03/2019.
+//  Copyright © 2019 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("we are here ")
+        
+        navigationItem.title = "Home"
+        
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+
+    }
+
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150.0)
+    }
+}
+
